769d2fd84e885248de0a1191d25456c395897c9d
Harithmech <harithmech@gmail.com>
2021-08-27 20:25:36 +0530

    iterative approach

--- array/reversearray.py
+++ array/reversearray.py
@@ -2,5 +2,14 @@
 
 
 # simple approach
-arr = [1, 2, 3, 4, 5]
+""" arr = [1, 2, 3, 4, 5]
 print(arr[::-1])
+ """
+
+# using iteration
+arr = [1, 2, 3, 4, 5]
+n = len(arr)
+for i in range(0, n//2):
+    print(arr[i], arr[n-1-i])
+    arr[i], arr[n-i-1] = arr[n-i-1], arr[i]
+print(arr)
